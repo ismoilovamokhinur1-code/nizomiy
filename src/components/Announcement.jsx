9c13c3222593cc37cea8dbeb66b8ef00bd91adb1
ismoilovamokhinur1-code <ismoilovamokhinur1@gmail.com>
2025-09-24 23:09:47 +0500

    hello

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,127 +1,138 @@
 import React from 'react';
 import announce from "../assets/announce.svg";
-import location from "../assets/location.svg"
+import location from "../assets/location.svg";
 import { Link } from 'react-router-dom';
 import { useTranslation } from "react-i18next";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 export default function Announcement() {
     const { t } = useTranslation();
-    
-    return (
-        <>
-            <div className="announce-wrap">
-                <div className='flex justify-between'>
-                    <div className='flex gap-4'>
-                        <img src={announce} alt="announce" className='animate-spin-fast'/>
-                        <p className='text-[40px] font-medium'>{t("announce_title")}</p>
-                    </div>
 
-                    <div>
-                       <Link to="/announce">
-                        <button className='w-[171px] h-[53px] text-white announce-btn'>{t("announce.btn")}</button>
-                       </Link>
-                    </div>
+    return (
+        <div className="announce-wrap">
+            {/* header */}
+            <div className='flex justify-between items-center mb-6'>
+                <div className='flex gap-4 items-center'>
+                    <img src={announce} alt="announce" className='animate-spin-fast' />
+                    <p className='text-[40px] font-medium'>{t("announce_title")}</p>
                 </div>
 
-                {/* cards */}
-                <div className='flex justify-between wrap-cards'>
+                <Link to="/announce">
+                    <button className='w-[171px] h-[53px] text-white announce-btn'>
+                        {t("announce.btn")}
+                    </button>
+                </Link>
+            </div>
+
+            {/* swiper */}
+            <div className='swiper'>
+                <Swiper
+                    modules={[Autoplay]}
+                    spaceBetween={20}
+                    slidesPerView={3}
+                    loop={true}
+                    autoplay={{
+                        delay: 2000,
+                        disableOnInteraction: false,
+                    }}
+                    breakpoints={{
+                        320: { slidesPerView: 1 },
+                        768: { slidesPerView: 2 },
+                        1024: { slidesPerView: 3 }
+                    }}
+                >
                     {/* card 1 */}
-                    <div className='w-[320px] h-[354px]'>
-
-                        <div className='bg-[#086D7A] card-first'>
-                            <p>{t("announce.date")}</p>
-                            <h1 className='text-[20px] font-medium'>{t("announce.title1")} <br /> {t("announce.title2")}</h1>
-                        </div>
-
-                        <div>
+                    <SwiperSlide>
+                        <div className='w-[320px] h-[354px]'>
+                            <div className='bg-[#086D7A] card-first'>
+                                <p>{t("announce.date")}</p>
+                                <h1 className='text-[20px] font-medium'>
+                                    {t("announce.title1")} <br /> {t("announce.title2")}
+                                </h1>
+                            </div>
                             <div className='bg-[#F3F7FD] card-second'>
                                 <div className='flex gap-2'>
-                                    <img src={location} alt="img" className='w-[20px] h-[20px]'/>
-                                <h1>{t("announce.place")}</h1>
+                                    <img src={location} alt="img" className='w-[20px] h-[20px]' />
+                                    <h1>{t("announce.place")}</h1>
                                 </div>
                                 <p className='text-[#121212]'>{t("announce.desc")}</p>
-                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>{t("announce.register")}</button>
+                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>
+                                    {t("announce.register")}
+                                </button>
                             </div>
                         </div>
-
-                    </div>
-
-                    {/* card 1 */}
-
-
-                           {/* card 2 */}
-                    <div className='w-[320px] h-[354px]'>
-
-                        <div className='bg-[#195A91] card-first'>
-                            <p>{t("announce.date")}</p>
-                            <h1 className='text-[20px] font-medium'>{t("announce.title1")} <br /> {t("announce.title2")}</h1>
-                        </div>
-
-                        <div>
-                            <div className='bg-[#F3F7FD] card-second'>
-                                <div className='flex gap-2'>
-                                    <img src={location} alt="img" className='w-[20px] h-[20px]'/>
-                                <h1>{t("announce.place")}</h1>
-                                </div>
-                                <p className='text-[#121212]'>{t("announce.desc")}</p>
-                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>{t("announce.register")}</button>
-                            </div>
-                        </div>
-
-                    </div>
+                    </SwiperSlide>
 
                     {/* card 2 */}
-
-                           {/* card 3 */}
-                    <div className='w-[320px] h-[354px]'>
-
-                        <div className='bg-[#195A91] card-first'>
-                            <p>{t("announce.date")}</p>
-                            <h1 className='text-[20px] font-medium'>{t("announce.title1")} <br /> {t("announce.title2")}</h1>
-                        </div>
-
-                        <div>
+                    <SwiperSlide>
+                        <div className='w-[320px] h-[354px]'>
+                            <div className='bg-[#195A91] card-first'>
+                                <p>{t("announce.date")}</p>
+                                <h1 className='text-[20px] font-medium'>
+                                    {t("announce.title1")} <br /> {t("announce.title2")}
+                                </h1>
+                            </div>
                             <div className='bg-[#F3F7FD] card-second'>
                                 <div className='flex gap-2'>
-                                    <img src={location} alt="img" className='w-[20px] h-[20px]'/>
-                                <h1>{t("announce.place")}</h1>
+                                    <img src={location} alt="img" className='w-[20px] h-[20px]' />
+                                    <h1>{t("announce.place")}</h1>
                                 </div>
                                 <p className='text-[#121212]'>{t("announce.desc")}</p>
-                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>{t("announce.register")}</button>
+                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>
+                                    {t("announce.register")}
+                                </button>
                             </div>
                         </div>
-
-                    </div>
+                    </SwiperSlide>
 
                     {/* card 3 */}
-
-
-                           {/* card 4 */}
-                    <div className='w-[320px] h-[354px]'>
-
-                        <div className='bg-[#086D7A] card-first'>
-                            <p>{t("announce.date")}</p>
-                            <h1 className='text-[20px] font-medium'>{t("announce.title1")} <br /> {t("announce.title2")}</h1>
-                        </div>
-
-                        <div>
+                    <SwiperSlide>
+                        <div className='w-[320px] h-[354px]'>
+                            <div className='bg-[#195A91] card-first'>
+                                <p>{t("announce.date")}</p>
+                                <h1 className='text-[20px] font-medium'>
+                                    {t("announce.title1")} <br /> {t("announce.title2")}
+                                </h1>
+                            </div>
                             <div className='bg-[#F3F7FD] card-second'>
                                 <div className='flex gap-2'>
-                                    <img src={location} alt="img" className='w-[20px] h-[20px]'/>
-                                <h1>{t("announce.place")}</h1>
+                                    <img src={location} alt="img" className='w-[20px] h-[20px]' />
+                                    <h1>{t("announce.place")}</h1>
                                 </div>
                                 <p className='text-[#121212]'>{t("announce.desc")}</p>
-                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>{t("announce.register")}</button>
+                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>
+                                    {t("announce.register")}
+                                </button>
                             </div>
                         </div>
-
-                    </div>
+                    </SwiperSlide>
 
                     {/* card 4 */}
-                </div>
-                {/* cards */}
-
+                    <SwiperSlide>
+                        <div className='w-[320px] h-[354px]'>
+                            <div className='bg-[#086D7A] card-first'>
+                                <p>{t("announce.date")}</p>
+                                <h1 className='text-[20px] font-medium'>
+                                    {t("announce.title1")} <br /> {t("announce.title2")}
+                                </h1>
+                            </div>
+                            <div className='bg-[#F3F7FD] card-second'>
+                                <div className='flex gap-2'>
+                                    <img src={location} alt="img" className='w-[20px] h-[20px]' />
+                                    <h1>{t("announce.place")}</h1>
+                                </div>
+                                <p className='text-[#121212]'>{t("announce.desc")}</p>
+                                <button className='text-[14px] w-[113px] h-[32px] text-[#195A91]'>
+                                    {t("announce.register")}
+                                </button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
             </div>
-        </>
-    )
+        </div>
+    );
 }
